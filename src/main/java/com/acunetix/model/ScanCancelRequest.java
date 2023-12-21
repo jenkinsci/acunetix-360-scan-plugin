5ebefa18a5491ce3c13023f99b6cdb994b48d66b
Burak İSPİR <burak.ispir@invicti.com>
2023-12-21 16:46:39 +0300

    Updated scan cancel

--- src/main/java/com/acunetix/model/ScanCancelRequest.java
+++ src/main/java/com/acunetix/model/ScanCancelRequest.java
@@ -2,28 +2,38 @@ package com.acunetix.model;
 
 import hudson.util.Secret;
 import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ContentType;
 
 public class ScanCancelRequest extends ScanRequestBase {
     public ScanCancelRequest(String apiURL, Secret apiToken,String scanTaskId) throws MalformedURLException, NullPointerException, URISyntaxException {
         super(apiURL, apiToken);
         this.scanTaskId = scanTaskId;
         this.scanCancelUri = new URL(ApiURL, "api/1.0/scans/CancelScanForPlugin/").toURI();
+        this.contentLength = "0";
+        this.content = "";
     }
 
     public final String scanTaskId;
     public final URI scanCancelUri;
+    public final String contentLength;
+    public final String content;
 
     public ClassicHttpResponse scanCancelRequest() throws IOException {
-        HttpClient client = getHttpClient();
-        final HttpPost httpPost = new HttpPost(scanCancelUri + scanTaskId);
+        CloseableHttpClient client = getDefaultHttpClient();
+        HttpPost httpPost = new HttpPost(scanCancelUri + scanTaskId);
+        HttpEntity stringEntity = new StringEntity(content,ContentType.APPLICATION_JSON);
+        httpPost.setEntity(stringEntity);
         httpPost.setHeader("Accept", json);
         httpPost.setHeader(HttpHeaders.AUTHORIZATION, getAuthHeader());
 
